ddfb247bcec4bceca4a373f388fa38dc5b27d9c1
Rahul Adsure <adsurerahul96@gmail.com>
2022-04-19 14:26:24 +0530

    Added: refactored code for delegate and protocol

--- ProtocolDelegate/FirstVC.swift
+++ ProtocolDelegate/FirstVC.swift
@@ -9,32 +9,18 @@ import UIKit
 
 class FirstVC: UIViewController,UserDataProtocol {
    
-    
-    
-    
-
     @IBOutlet weak var LabelOne: UILabel!
     
     @IBOutlet weak var LabelTwo: UILabel!
     
     @IBAction func GotoButtonClicked(_ sender: UIButton) {
-       
-        
         let selectionVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") as! SecondVC
         selectionVC.delegate = self
         self.navigationController?.pushViewController(selectionVC, animated: true)
-//                present(selectionVC, animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-      
     }
-//    func passingDetail(firstName: String, lastName: String) {
-//        LabelOne.text = firstName
-//        LabelTwo.text = lastName
-//
-//    }
     func passingDetail(object: [String : String]) {
         LabelOne.text = object["firstName"]
         LabelTwo.text = object["lastName"]
--- ProtocolDelegate/SecondVC.swift
+++ ProtocolDelegate/SecondVC.swift
@@ -7,28 +7,19 @@
 
 import UIKit
 protocol UserDataProtocol{
-//    func passingDetail(firstName: String, lastName:String)
     func passingDetail(object:[String:String])
 }
 class SecondVC: UIViewController {
-  
-    
-    
-    var delegate:UserDataProtocol!
+   var delegate:UserDataProtocol!
     
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-print("second screen")
-        
     }
     
-
     @IBAction func goToMainButtonClick(_ sender: UIButton) {
-//        delegate.passingDetail(firstName: firstName.text!, lastName: lastName.text!)
         let detailDic:[String:  String] = ["firstName":firstName.text!,"lastName":lastName.text!]
-        
         delegate.passingDetail(object: detailDic)
     }
     
